fda131e9c22d00deedd46e442a8b48349fa81efb
kietran99 <kietran99@gmail.com>
2021-09-01 15:51:48 +0700

    + Implement BT Task Wait behavior

--- Assets/RR_BehaviorTree/Scripts/Editor/Builtin_Task/BTTaskWait.cs
+++ Assets/RR_BehaviorTree/Scripts/Editor/Builtin_Task/BTTaskWait.cs
@@ -8,11 +8,19 @@ namespace RR.AI.BehaviorTree
 
         public override void Init(GameObject actor, Blackboard blackboard, BTTaskWaitData prop)
         {
-            
+            prop.Elapsed = 0f;
         }
 
         public override BTNodeState Tick(GameObject actor, Blackboard blackboard, BTTaskWaitData prop)
-        {
+        {   
+            prop.Elapsed += Time.deltaTime;
+
+            if (prop.Elapsed < prop.Duration)
+            {
+                return BTNodeState.FAILURE;
+            }
+
+            prop.Elapsed = 0f;
             return BTNodeState.SUCCESS;
         }
     }
@@ -21,5 +29,7 @@ namespace RR.AI.BehaviorTree
 	public class BTTaskWaitData
 	{
 		public float Duration;
+
+        public float Elapsed { get; set; }
 	}
 }

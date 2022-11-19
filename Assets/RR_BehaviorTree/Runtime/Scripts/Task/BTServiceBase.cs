689bf9ff7f163fe277199bcbdaae7c5089b0d9d9
kietran99 <kietran99@gmail.com>
2022-11-19 16:59:19 +0700

    * Fix BT Service random deviation logic - Refactor

--- Assets/RR_BehaviorTree/Runtime/Scripts/Task/BTServiceBase.cs
+++ Assets/RR_BehaviorTree/Runtime/Scripts/Task/BTServiceBase.cs
@@ -9,28 +9,39 @@ namespace RR.AI.BehaviorTree
         public float TickInterval;
         public float RandomDeviation = 0.0f;
 
-        private float counter = 0.0f;
-        private float minInterval, maxInterval;
-
+        private float _counter = 0.0f;
+        private float _minInterval, _maxInterval;
+        
         public float Elapsed { get; protected set; }
 
         public void Reset()
         {
             Elapsed = 0.0f;
-            counter = 0.0f;
-            minInterval = TickInterval - RandomDeviation;
-            maxInterval = TickInterval + RandomDeviation;
+            _counter = 0.0f;
+            _minInterval = TickInterval - RandomDeviation;
+            _maxInterval = TickInterval + RandomDeviation;
         }
 
         public bool Tick(float deltaTime)
         {
+            _counter += deltaTime;
             Elapsed += deltaTime;
-            counter += deltaTime;
 
-            if ((RandomDeviation == 0.0f && counter >= TickInterval)
-                || (counter >= minInterval && counter <= maxInterval))
+            if (RandomDeviation == 0.0f)
             {
-                counter = 0.0f;
+                if (_counter >= TickInterval)
+                {
+                    _counter = 0.0f;
+                    return true;
+                }
+
+                return false;
+            }
+
+            float curInterval = Random.Range(0, 2) == 0 ? _minInterval : _maxInterval;
+            if (_counter >= curInterval)
+            {
+                _counter = 0.0f;
                 return true;
             }
 

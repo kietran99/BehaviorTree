1e6591025cb15070d205dc19817e61b2e91b4172
kietran99 <kietran99@gmail.com>
2022-11-20 09:57:45 +0700

    - Extract BTPropServiceBase to separate file

--- Assets/RR_BehaviorTree/Runtime/Scripts/Task/BTServiceBase.cs
+++ Assets/RR_BehaviorTree/Runtime/Scripts/Task/BTServiceBase.cs
@@ -4,51 +4,6 @@ namespace RR.AI.BehaviorTree
 {
     public interface IBTService {}
 
-    public abstract class BTPropServiceBase
-    {
-        public float TickInterval;
-        public float RandomDeviation = 0.0f;
-
-        private float _counter = 0.0f;
-        private float _minInterval, _maxInterval;
-        
-        public float Elapsed { get; protected set; }
-
-        public void Reset()
-        {
-            Elapsed = 0.0f;
-            _counter = 0.0f;
-            _minInterval = TickInterval - RandomDeviation;
-            _maxInterval = TickInterval + RandomDeviation;
-        }
-
-        public bool Tick(float deltaTime)
-        {
-            _counter += deltaTime;
-            Elapsed += deltaTime;
-
-            if (RandomDeviation == 0.0f)
-            {
-                if (_counter >= TickInterval)
-                {
-                    _counter = 0.0f;
-                    return true;
-                }
-
-                return false;
-            }
-
-            float curInterval = Random.Range(0, 2) == 0 ? _minInterval : _maxInterval;
-            if (_counter >= curInterval)
-            {
-                _counter = 0.0f;
-                return true;
-            }
-
-            return false;
-        }
-    }
-
     public abstract class BTServiceBase<T> : BTBaseTask<T>, IBTService where T : BTPropServiceBase, new()
     {
         protected abstract void OnStart(GameObject actor, RuntimeBlackboard blackboard, T prop);

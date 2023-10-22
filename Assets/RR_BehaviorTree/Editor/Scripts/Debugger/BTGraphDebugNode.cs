f747ac91f2a6e5baf52c6fb47e3ebdc5e9c612a2
kietran99 <kietran99@gmail.com>
2023-10-22 11:07:14 +0700

    - Prevent losing edges color & width in debugger * Fix missing edge debug visual

--- Assets/RR_BehaviorTree/Editor/Scripts/Debugger/BTGraphDebugNode.cs
+++ Assets/RR_BehaviorTree/Editor/Scripts/Debugger/BTGraphDebugNode.cs
@@ -14,7 +14,7 @@ namespace RR.AI.BehaviorTree.Debugger
         private const string CLASS_ACTIVE_BORDER = "active-border";
 
         private BTGraphNodeBase _decoratee;
-        private Port _inPort;
+        private readonly Port _inPort, _outPort;
         private Edge _inEdge;
         private BTGraphNodeBase _parentNode;
         private int _parentIdx;
@@ -22,6 +22,8 @@ namespace RR.AI.BehaviorTree.Debugger
         public BTGraphNodeBase ParentNode => _parentNode;
         public int ParentIdx => _parentIdx;
 
+        private readonly System.Action<object, object> SetInternalInEdgeWidth;
+        
         public BTGraphDebugNode(BTGraphNodeBase decoratee)
         {
             _decoratee = decoratee;
@@ -30,9 +32,13 @@ namespace RR.AI.BehaviorTree.Debugger
             if (_decoratee.inputContainer.childCount != 0)
             {
                 _inPort = _decoratee.inputContainer[0] as Port;
+                _outPort = _decoratee.outputContainer.childCount == 0 ? null : _decoratee.outputContainer[0] as Port;
                 foreach (var edge in _inPort.connections) // There should only be 1 edge
                 {
                     _inEdge = edge;
+                    SetInternalInEdgeWidth = edge.GetType()
+                        .GetField("m_EdgeWidth", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                        .SetValue;
                     _parentNode = edge.output.node as BTGraphNodeBase;
                     _parentIdx = _parentNode.OrderValue;
                     // edge.capabilities &= ~Capabilities.Selectable;
@@ -43,23 +49,26 @@ namespace RR.AI.BehaviorTree.Debugger
         public void Reset()
         {
             _decoratee.RemoveFromClassList(CLASS_ACTIVE_BORDER);
-            _inPort.portColor = DEFAULT_EDGE_COLOR;           
-            var edgeColor = DEBUG_INACTIVE_EDGE_COLOR;
-            _inEdge.edgeControl.edgeWidth = 3;
-            _inEdge.edgeControl.inputColor = edgeColor;
-            _inEdge.edgeControl.outputColor = edgeColor;
-            _inEdge.edgeControl.fromCapColor = _inEdge.edgeControl.inputColor;
-            _inEdge.edgeControl.toCapColor = _inEdge.edgeControl.outputColor;
+            SetEdgeColor(DEBUG_INACTIVE_EDGE_COLOR, 3);
         }
 
         public void Tick()
         {
             _decoratee.AddToClassList(CLASS_ACTIVE_BORDER);
-            _inPort.portColor = DEFAULT_EDGE_COLOR;
-            var edgeColor = DEBUG_ACTIVE_EDGE_COLOR;
-            _inEdge.edgeControl.edgeWidth = 6;
-            _inEdge.edgeControl.inputColor = edgeColor;
-            _inEdge.edgeControl.outputColor = edgeColor;
+            SetEdgeColor(DEBUG_ACTIVE_EDGE_COLOR, 6);
+        }
+
+        private void SetEdgeColor(Color color, int edgeWidth)
+        {
+            _inPort.portColor = color;
+            if (_outPort != null)
+            {
+                _outPort.portColor = color;
+            }
+
+            SetInternalInEdgeWidth(_inEdge, edgeWidth);
+            _inEdge.edgeControl.inputColor = color;
+            _inEdge.edgeControl.outputColor = color;
             _inEdge.edgeControl.fromCapColor = _inEdge.edgeControl.inputColor;
             _inEdge.edgeControl.toCapColor = _inEdge.edgeControl.outputColor;
         }
